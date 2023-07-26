735b9595edd0912d3e3e753a4fcbe2f9059b9f93
Miradzyapko <95940989+Miradzyapko@users.noreply.github.com>
2023-07-26 20:55:23 +0000

    Deploying to gh-pages from @ Miradzyapko/goit-react-hw-08-phonebook@803bc2deab915775b6e89b4e0a768e6da71da13a 🚀

--- src/components/ContactListItem/ContactListItem.Styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-export const ListItem = styled.li`
-
-color: blue;
-  margin-bottom: 5px;
-  font-size:16px;
-  align-items: center;
-  justify-content: center;
-`;
